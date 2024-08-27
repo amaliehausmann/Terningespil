e92bb5cb1ac295af7315cf0552bc4b00d82a06cb
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-08-27 15:00:49 +0200

    Knappe

--- terningespil/src/App.jsx
+++ terningespil/src/App.jsx
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
+import Button from './components/Buttons/Higher';
+import style from './components/Buttons/Higher.module.scss';
 import './App.scss'
 
 function App() {
-
+  const handleClick = () => {   
+};
   return (
     <>
+    <div className='AllButtons'>
+         <Button style = {style.Higher} higher={'Name'} onClick={handleClick}/>
+    </div>
     </>
   )
 }
-
 export default App
